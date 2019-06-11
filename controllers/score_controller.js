e9a7d49acfb5fa661f6d8b57f999a9c757bd30e4
Adrian Delpha <adrian.delpha@gmail.com>
2019-06-10 19:37:01 -0700

    Secret Key Auth

--- controllers/score_controller.js
+++ controllers/score_controller.js
@@ -1,4 +1,6 @@
 const Score = require('../models/score');
+const dotenv = require('dotenv').config();
+const authErrorMsg = 'Authentication Required.';
 
 exports.score_index = (req, res, next) => {
   Score.find({}, function(err, scores){
@@ -9,16 +11,20 @@ exports.score_index = (req, res, next) => {
   })
 }
 exports.score_create = (req, res, next) => {
-  let score = new Score({
-    name: req.query.name || 'Cadet',
-    score: req.query.score
-  });
-  score.save(function (err){
-    if(err){
-      return next(err);
-    }
-    res.send('Score Added' + score.name + ':' + score.score);
-  })
+  if(req.query.secret === process.env.SECRET_KEY){
+    let score = new Score({
+      name: req.query.name || 'Cadet',
+      score: req.query.score
+    });
+    score.save(function (err){
+      if(err){
+        return next(err);
+      }
+      res.send('Score Added' + score.name + ':' + score.score);
+    })
+  }else{
+    res.send(authErrorMsg);
+  }
 }
 exports.score_details = (req, res, next) =>  {
   Score.findById(req.params.id, function(err, score){
@@ -29,26 +35,34 @@ exports.score_details = (req, res, next) =>  {
   })
 };
 exports.score_update = (req, res, next) =>  {
-  let updateObj = {};
-  if(req.query.name){
-    updateObj.name = req.query.name;
-  }
-  if(req.query.score){
-    updateObj.score = req.query.score;
-  }
-  Score.findByIdAndUpdate(req.params.id, {$set: updateObj}, function(err, score){
-    if(err){
-      return next(err);
+  if(req.query.secret === process.env.SECRET_KEY){
+    let updateObj = {};
+    if(req.query.name){
+      updateObj.name = req.query.name;
     }
-    res.send('Score Updated');
-  })
+    if(req.query.score){
+      updateObj.score = req.query.score;
+    }
+    Score.findByIdAndUpdate(req.params.id, {$set: updateObj}, function(err, score){
+      if(err){
+        return next(err);
+      }
+      res.send('Score Updated');
+    })
+  }else{
+    res.send(authErrorMsg);
+  }
 };
 exports.score_delete = (req, res, next) =>  {
-  Score.findByIdAndRemove(req.params.id, function(err, score){
-    if(err){
-      return next(err);
-    }
-    res.send('Score Deleted' + ' ' + req.params.id);
-  })
+  if(req.query.secret === process.env.SECRET_KEY){
+    Score.findByIdAndRemove(req.params.id, function(err, score){
+      if(err){
+        return next(err);
+      }
+      res.send('Score Deleted' + ' ' + req.params.id);
+    })
+  }else{
+    res.send(authErrorMsg);
+  }
 }
 
